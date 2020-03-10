9d0b2cf409f876f9d758f7dd54fe8ed750a633f6
Vivica Ma <vivicacongma@gmail.com>
2020-03-10 22:13:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,13 @@
 # Make your shoe class here!
 class Shoe 
   attr_accessor :brand
+  attr_reader :color 
 def initialize(brand)
   @brand = brand 
+end 
+
+def color=(color)
+  @color = color
 end 
 
 end 
