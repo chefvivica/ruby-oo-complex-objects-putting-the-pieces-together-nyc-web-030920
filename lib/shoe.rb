20505e5ac5b52058dac4d7f8e669c7db9f039767
Vivica Ma <vivicacongma@gmail.com>
2020-03-10 22:24:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,30 +2,27 @@
 class Shoe 
   attr_accessor :brand
   attr_reader :color, :size, :material, :condition
-def initialize(brand)
-  @brand = brand 
-end 
-
-def color=(color)
-  @color = color
-end 
-
-def size=(size)
-  @size = size
-end 
-def material=(material)
-  @material = material
-end 
-
-def condition=(condition)
-  @condition = condition 
-end 
-
-def cobble 
-  @condition = "new"
-  puts "Your shoe is as good as new!"
-
-end 
-
-
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def color=(color)
+    @color = color
+  end 
+  
+  def size=(size)
+    @size = size
+  end 
+  def material=(material)
+    @material = material
+  end 
+  
+  def condition=(condition)
+    @condition = condition 
+  end 
+  
+  def cobble 
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end 
 end 
