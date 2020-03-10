af7ef7d51bffa76cdf86ae7e072035b5923dd5d3
Vivica Ma <vivicacongma@gmail.com>
2020-03-10 22:05:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,30 +7,7 @@ class Book
   def title 
     @title 
   end 
-  
-  def author=(author)
-    @author = author 
-  end 
-  
-  def author 
-    @author
-  end 
-  
-  def page_count=(num)
-    @page_count = num
-  end 
-  
-  def page_count
-    @page_count
-  end 
-  
-  def genre=(type)
-    @genre = type 
-  end 
-  
-  def genre
-    @genre 
-  end 
+
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
